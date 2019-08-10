5bbc555bf05b2b62746933cb539fdce5e99076ee
mumtaz khan <mumtaz.khan@kubota.com>
2019-08-10 10:34:50 -0500

    comments about service discovery mapping

--- src/main/java/com/sparkarabic/registryservice/client/PersonClient.java
+++ src/main/java/com/sparkarabic/registryservice/client/PersonClient.java
@@ -6,7 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@FeignClient(name = "persons-svc")
+@FeignClient(name = "persons-svc") // this name should be same as
+// service name of person project e.g.
+//         persons-svc             ClusterIP      10.100.184.33    <none>        80/TCP
+// note this is not the load balancer
 public interface PersonClient {
     @GetMapping
     public List<Person> getAll();
